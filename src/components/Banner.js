45776f3f50bf2a6ec0217500edb329734bef7812
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2025-07-23 17:43:01 -0800

    Modified Banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/header-img.JPG";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = [ "Software Developer", "AI Researcher", "Computer Engineer" ];
+    const toRotate = [ "Software Developer", "AI Research Assistant", "Computer Engineer", "SWE Club VP" ];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -46,12 +46,17 @@ export const Banner = () => {
                 <Row className="align-item-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Mahri `}<span className="wrap">{text}</span></h1>
-                        <p>I'm Mahriban Yalkapova, a Computer Science student at the University of Washington. I have a strong passion for technology, especially in areas like AI and quantum computing. My goal is to leverage my skills to contribute to impactful projects and continue growing in the tech industry.</p>
-                        <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25} /></button>
+                        <h1>{`Hi I'm Mahri, `}<span className="wrap">{text}</span></h1>
+                        <p>I'm Mahriban Yalkapova, a Computer Science and Engineering student at the University of Washington. 
+                            I have a strong passion for technology, especially in areas like AI and quantum computing. 
+                            My goal is to leverage my skills to contribute to impactful projects and continue growing in the tech industry.</p>
+                        <button onClick={() => window.open('/resume.pdf', '_blank')}>
+                            My Resume <ArrowRightCircle size={25} /></button>
+
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img" />
+                        <img src={headerImg} alt="Header Img" 
+                        style={{ width: "400px", height: "400px", borderRadius: "50%", objectFit: "cover" }} />
                     </Col>
                 </Row>
             </Container>
