9cbd08c2032d7d221e2d51fee632b2ec52cf2f41
cyberwiz <115326337+MahriYalkapova@users.noreply.github.com>
2025-09-09 13:34:57 -0700

    Refined Experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,40 +1,46 @@
 import skills from "../data/skills.json";
 import history from "../data/history.json";
-import styles from "./Experience.css"
+import './Experience.css';
 import htmlImg from "../assets/img/html.png";
 import cssImg from "../assets/img/css.png";
+import nodeImg from "../assets/img/node.png";
 import reactImg from "../assets/img/react.png";
 import figmaImg from "../assets/img/figma.png";
-import nodeImg from "../assets/img/node.png";
+import javaImg from "../assets/img/java.png";
+import pythonImg from "../assets/img/python.png";
+import verilogImg from "../assets/img/verilog.png";
 
 const skillImages = {
-  htmlImg,
-  cssImg,
-  reactImg,
-  figmaImg,
-  nodeImg
+  "skills/html.png": htmlImg,
+  "skills/css.png": cssImg,
+  "skills/node.png": nodeImg,
+  "skills/react.png": reactImg,
+  "skills/figma.png": figmaImg,
+  "skills/python.png": pythonImg,
+  "skills/java.png": javaImg,
+  "skills/verilog.png": verilogImg,
 };
 
 export const Experience = () => {
     return (
-        <section className={styles.container} id="experience">
-        <h2 className={styles.title}>Experience</h2>
-        <div className={styles.content}>
-            <div className={styles.skills}>{
+        <section className="container" id="experience">
+        <h2 className="title">Experience</h2>
+        <div className="content">
+            <div className="skills">{
                 skills.map((skill, id) => {
-                    return <div key={id} className={styles.skill}>
-                        <div className={styles.skillImageContainer}>
-                            <img src={skillImages[skill.imageSrc]} alt={skill.title}/>
+                    return <div key={id} className="skill">
+                        <div className="skillImageContainer">
+                            <img src={skillImages[skill.imageSrc]} alt={skill.title} />
                         </div>
                         <p>{skill.title}</p>
                     </div>
                 })
                 }</div>
-            <ul className={styles.history}>
+            <ul className="history">
                 {
                     history.map((historyItem, id) => {
-                        return <li key={id} className={styles.historyItem}>
-                            <div className={styles.historyItemDetails}>
+                        return <li key={id} className="historyItem">
+                            <div className="historyItemDetails">
                                 <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                                 <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                                 <ul>{historyItem.experiences.map((experience, id) => {
